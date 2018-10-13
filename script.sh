b220e5381c78f1ce66d5896ce1b2c8de4d22539d
holowan <holowanl@gmail.com>
2018-10-12 23:12:36 -0700

    bash

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+read -p "Enter name: " name
+
+
+if [ $name = "Will" ]
+then
+        echo -n "Greetings, Will. Enter password: "
+        read -s passwrd
+        echo
+
+count=$(echo -n $passwrd | wc -c)
+
+        if [ $count -lt 8 ]
+        then
+                echo "$count characters is too short for a password. You need at least 8."
+        else
+                echo "Access granted. You're water bottle is the biggest."        
+        fi
+elif [ $name = "Kathleen" ]
+then
+        echo "$name, you are quite good at turning me on."
+
+else
+        echo "$name, you are not an authorized user."
+fi
+
+
